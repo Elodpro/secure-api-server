212e2a3ece8a2ff2894425deb95d6748b6f4e8c9
Elodpro <elod.pro1@gmail.com>
2025-11-04 16:30:04 +0100

    Ajout du fichier "AuthController.java"

---
+++ src/main/java/com/serweb/secure_api_server/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.serweb.secure_api_server.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Contrôleur dédié à l'authentification
+ * C'est ici que le client viendra demander un jeton JWT
+ *
+ **/
+
+@RestController
+public class AuthController {
+
+    /**
+     * Point de terminaison pour l'authentification (login).
+     * Le client enverra ses identifiants (login/mdp) ici.
+     */
+
+    @PostMapping("/auth/login") // C'est un POST, pas un GET
+    public Map<String, String> login() {
+        // C'est ici qu'on va générer le JWT
+        return Map.of("message", "Login Successful. JWT generation pending");
+    }
+}
